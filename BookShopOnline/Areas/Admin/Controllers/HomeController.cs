9b67984b637dce433cd440acc7bccc7d777ae8e9
vuthuan <vuthuan3090@gmail.com>
2020-11-24 09:26:50 +0700

    push all project

---
+++ BookShopOnline/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using BookShopOnline.Areas.Admin.Models;
+
+namespace BookShopOnline.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookStoreDbContext db = new BookStoreDbContext();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }                
+    }
+}
